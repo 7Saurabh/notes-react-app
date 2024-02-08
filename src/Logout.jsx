97f9d712172304eb427b2d32db25b222becf3c75
Saurabh Mhatre <93332937+7Saurabh@users.noreply.github.com>
2024-02-08 17:16:37 +0530

    Update Logout.jsx

--- src/Logout.jsx
+++ src/Logout.jsx
@@ -6,7 +6,7 @@ function Logout(){
     const navigate = useNavigate()
 
     useEffect(() => {
-        fetch('http://127.0.0.1:5000/api/logout', {
+        fetch('https://nerch26.pythonanywhere.com/api/logout', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
